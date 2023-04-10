3b0f624dbc40a3ebd8feb6f60f76dd0d9bbac14c
cody <phamgcody01@gmail.com>
2023-04-09 22:57:47 -0400

    Create function for basic math operators

---
+++ scripts.js
@@ -0,0 +1,15 @@
+function add(currentNumber, newNumber) {
+    return currentNumber+newNumber;
+}
+
+function subtract(currentNumber, newNumber) {
+    return currentNumber-newNumber;
+}
+
+function multiply(currentNumber, newNumber) {
+    return currentNumber*newNumber;
+}
+
+function divide(currentNumber, newNumber) {
+    return currentNumber/newNumber;
+}
