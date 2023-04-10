8fa030eed12f7914cb2392321173c3b559ba7306
cody <phamgcody01@gmail.com>
2023-04-09 23:48:52 -0400

    Add variables for numbers and make operate function

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 function add(currentNumber, newNumber) {
-    return currentNumber+newNumber;
+    return console.log(currentNumber+newNumber);
 }
 
 function subtract(currentNumber, newNumber) {
@@ -11,5 +11,26 @@ function multiply(currentNumber, newNumber) {
 }
 
 function divide(currentNumber, newNumber) {
-    return currentNumber/newNumber;
+    return console.log(currentNumber/newNumber);
 }
+
+function operate(currentNumber, newNumber, operator) {
+    if (operator === "+") {
+        add(currentNumber, newNumber);
+    }
+    else if (operator === "-") {
+        subtract(currentNumber, newNumber);
+    }
+    else if (operator === "x") {
+        multiply(currentNumber, newNumber);
+    }
+    else if (operator === "÷") {
+        divide(currentNumber, newNumber);
+    }
+}
+
+let initialValue = 0;
+let operator = "";
+let secondaryValue = 0;
+let displayValue = 0;
+
